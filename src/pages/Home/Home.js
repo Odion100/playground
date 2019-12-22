3b65186d0fd21e698b5394ad78b79b5ed62d91b7
Odion100 <odionedwards@gmail.com>
2019-12-22 17:51:08 -0500

    added message icon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.scss";
 import mapImage from "../../assets/map.png";
+import dmIcon from "../../assets/message.png";
 import BasketballGlobe from "../../atoms/BasketballGlobe/BasketballGlobe";
 import PhoneScreen from "../../atoms/PhoneScreen/PhoneScreen";
 const Home = props => {
@@ -9,12 +10,14 @@ const Home = props => {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-6">
-            <div className="home__phone1 phone">
+            <PhoneScreen bgImage={mapImage}>
               <div className="navbar">
                 <BasketballGlobe />
+                <div className="dm-icon">
+                  <img src={dmIcon} alt="chat" />
+                </div>
               </div>
-              <img className="home__mobile-map" src={mapImage} alt="Map" />
-            </div>
+            </PhoneScreen>
           </div>
           <div className="col-6">
             <PhoneScreen bgImage={mapImage}>
