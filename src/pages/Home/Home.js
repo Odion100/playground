21429e6217856a2e8c5573ff0aa03a3398e1e4ef
Odion100 <odionedwards@gmail.com>
2019-12-22 21:59:35 -0500

    added newsfeed icon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles.scss";
 import mapImage from "../../assets/map.png";
-
+import calloutsIcon from "../../assets/callouts.jpeg";
+import newsfeedIcon from "../../assets/newsfeed.png";
 import BasketballGlobe from "../../atoms/BasketballGlobe/BasketballGlobe";
 import PhoneScreen from "../../atoms/PhoneScreen/PhoneScreen";
 import MessageIcon from "../../atoms/MessageIcon/MessageIcon";
+
 const Home = props => {
   return (
     <section className="home">
@@ -24,6 +26,27 @@ const Home = props => {
             <PhoneScreen bgImage={mapImage}>
               <div className="navbar">
                 <BasketballGlobe />
+              </div>
+              <div className="screen">
+                <div>This is the screen</div>
+              </div>
+              <div className="bottom-panel">
+                <div className="callout-icon">
+                  <div className="notification-badge">23</div>
+                  <img
+                    className="callout-icon__icon"
+                    src={calloutsIcon}
+                    alt="Callouts"
+                  />
+                </div>
+                <div className="newsfeed-icon">
+                  <div className="notification-badge">23</div>
+                  <img
+                    className="newsfeed-icon__icon"
+                    src={newsfeedIcon}
+                    alt="Callouts"
+                  />
+                </div>
               </div>
             </PhoneScreen>
           </div>
