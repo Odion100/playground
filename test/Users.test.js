8568f62a12c24ac9e1c38580cd110b080cae1a36
odion100 <odionedwards@gmail.com>
2020-03-23 12:43:17 -0400

    completed Users.add test

--- test/Users.test.js
+++ test/Users.test.js
@@ -7,12 +7,17 @@ const url = `http://localhost:${port}/${route}`;
 describe("buAPI.Users.add(options)", () => {
   it("Successful request: Create a new user", async () => {
     const { Users } = await Client.loadService(url);
-    console.log("Users:", Users);
+
     const res = await Users.add({
       email: `odzysofreezy${parseInt(Math.random() * 100)}@gmail.com`,
       password: "passthesenutson"
     });
-    console.log(res);
-    expect(res).to.be.an("object");
+
+    expect(res)
+      .to.be.an("object")
+      .that.has.all.keys("newUser", "status", "message");
+    expect(res.message).to.be.equal("New user created successfully.");
+    expect(res.status).to.be.equal(200);
+    expect(res.newUser).to.be.an("object");
   });
 });
