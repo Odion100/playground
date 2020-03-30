6f06cd2ba43b0fc6c64daae1a7aef9c90fb98c0f
odion100 <odionedwards@gmail.com>
2020-03-30 11:29:43 -0400

    updated users test

--- test/Users.test.js
+++ test/Users.test.js
@@ -40,20 +40,66 @@ describe("buAPI.Users.get(options)", () => {
 
 describe("buAPI.Users.updateField(options)", () => {
   it("should be able to succesfully retrieve a user using email and password or just id", async () => {
+    const first_name = "Izak";
+    const last_name = "Chapper";
+    const age = 32;
+    const gender = "Male";
+    const profile_image = "image.png";
+    const primary_zipcodes = ["12133"];
+    const tag = "#kjdlsfj";
     const { Users } = await Client.loadService(url);
     const { user } = await Users.get({ email, password });
+    //console.log("user", user, email);
+    expect(user).to.be.an("object");
+    expect(user).to.have.property("email", email);
+    expect(user).to.have.property("account_status", "active");
+    expect(user)
+      .to.have.property("primary_zipcodes")
+      .that.is.an("array")
+      .that.has.a.lengthOf(0);
+    expect(user)
+      .to.have.property("created_date")
+      .that.is.a("string");
+
     const res = await Users.update({
       fields: {
-        first_name: "Izak",
-        last_name: "Chapper",
-        age: 32,
-        gender: "Male",
-        profile_image: "image.png",
-        primary_zipcodes: ["12133"],
-        tag: "#kjdlsfj"
+        first_name,
+        last_name,
+        age,
+        gender,
+        profile_image,
+        primary_zipcodes,
+        tag
       },
       id: user._id
     });
+    //console.log("res----->", res);
+    expect(res).to.be.an("object");
+    expect(res).to.have.property("status", 200);
+    expect(res)
+      .to.have.property("updatedUser")
+      .that.is.an("object");
+    expect(res.updatedUser).to.have.property("first_name", first_name);
+    expect(res.updatedUser).to.have.property("last_name", last_name);
+    expect(res.updatedUser).to.have.property("age", age);
+    expect(res.updatedUser).to.have.property("gender", gender);
+    expect(res.updatedUser).to.have.property("profile_image", profile_image);
+    expect(res.updatedUser)
+      .to.have.property("primary_zipcodes")
+      .that.deep.equals(primary_zipcodes);
+    expect(res.updatedUser).to.have.property("tag", tag);
+  });
+});
+
+describe("buAPI.Users.setAccountStatus(options)", () => {
+  it("should successfully update a given users accout_status", async () => {
+    const { Users } = await Client.loadService(url);
+    const { user } = await Users.get({ email, password });
+    const res = await Users.setAccountStatus({
+      status: "archived",
+      id: user._id
+    });
+    //console.log("Users.setAccountStatus---->", res);
     expect(res).to.be.an("object");
   });
 });
