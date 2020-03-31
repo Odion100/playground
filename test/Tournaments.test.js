352866b882aa1df86558f7b0e399569d2b88c6d8
odion100 <odionedwards@gmail.com>
2020-03-31 18:29:09 -0400

    added Tournaments.get test

--- test/Tournaments.test.js
+++ test/Tournaments.test.js
@@ -4,13 +4,14 @@ const route = "bu/api";
 const port = 7899;
 const url = `http://localhost:${port}/${route}`;
 
-const name = "FlyTournaments";
+const name = `FlyTournament-${parseInt(Math.random() * 100)}`;
 const root_admin = "5e82135037543ac757722727";
+const primary_zipcodes = ["12133"];
 
 describe("buAPI.Tournaments.add(options)", () => {
-  it("successfully create a new user with email and password required", async () => {
+  it("successfully create a new tournaments with email and password required", async () => {
     const { Tournaments } = await Client.loadService(url);
-    const res = await Tournaments.add({ name, root_admin });
+    const res = await Tournaments.add({ name, root_admin, primary_zipcodes });
 
     // console.log(res);
     expect(res)
@@ -25,9 +26,25 @@ describe("buAPI.Tournaments.add(options)", () => {
     expect(res.newTournament)
       .to.have.property("root_admin")
       .that.deep.equals(root_admin);
-    expect(res.newTournament).to.have.property("status", "unsubmitted");
-    expect(res.newTournament).to.have.property("referee", false);
+    expect(res.newTournament).to.have.property("status", "unpublished");
+    expect(res.newTournament).to.have.property("refereed", false);
     expect(res.newTournament).to.have.property("rounds", 1);
     expect(res.newTournament).to.have.property("round_clock", 0);
+  });
+});
+
+describe("buAPI.Tournaments.get(options)", () => {
+  it("should be able to succesfully retrieve a tournaments using email and password or just id", async () => {
+    const { Tournaments } = await Client.loadService(url);
+    const res = await Tournaments.get({ name });
+    console.log(res);
+    expect(res)
+      .to.be.an("object")
+      .that.has.all.keys("tournaments", "status");
+    expect(res.status).to.be.equal(200);
+    expect(res.tournaments)
+      .to.be.an("array")
+      .that.has.a.lengthOf(1);
+    expect(res.tournaments[0]).to.be.an("object");
   });
 });
